93abac9cd65c51824197300e34e8d79d528c8b48
tky-san <cist.b214.t.ito@gmail.com>
2024-03-12 00:25:45 +0900

    とりあえず一覧表示まで

---
+++ book/src/main/java/book/BookService.java
@@ -0,0 +1,26 @@
+package book;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import book.model.Book;
+import book.repository.BookRepository;
+
+@Service
+@Transactional
+public class BookService {
+	
+    @Autowired
+    BookRepository repository;
+	
+    /**
+     * データベースから本の一覧を取得する
+     * @return
+     */
+    public List<Book> findAll() {
+        return repository.findAll();
+    }
+}
