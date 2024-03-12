7d7a5b321e517f76569c152065a1b37d0151e0b5
tky-san <cist.b214.t.ito@gmail.com>
2024-03-12 23:29:35 +0900

    バリデーション機能を追加

--- book/src/main/java/book/form/BookForm.java
+++ book/src/main/java/book/form/BookForm.java
@@ -1,5 +1,8 @@
 package book.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
@@ -7,11 +10,16 @@ public class BookForm {
 
     /**
      * 本のタイトルを保持する
+     * チェック内容：空でないこと
      */
-    private String title;
+	@NotBlank(message="タイトルを入力してください。")
+	private String title;
 
     /**
      * 本の値段を保持する
+     * チェック内容：Nullでないこと、プラスであること
      */
-    private Integer price;
+	@NotNull(message="値段を入力してください。")
+    @Positive(message="値段はプラスの値を入力してください。")
+	private Integer price;
 }
